22a106aa69910c0adcaf91ae82f533d55f2a662b
Pro <Pro@DESKTOP-OBTOJB5>
2019-03-03 22:46:45 +0200

    v 1.12

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
- 
-import { Provider } from 'react-redux'
+import React, { Component } from "react";
 
-import { Main } from './src/screens'
-import { createStore } from './src/store'
-const store = createStore()
- 
+import { Provider } from "react-redux";
+
+import { Main } from "./src/screens";
+import { createStore } from "./src/store";
+const store = createStore();
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <Main />
-    </Provider>
+        <Main />
+      </Provider>
     );
   }
 }
